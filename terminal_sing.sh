afd7fec16216aea57862cc5fcf230f10ace70ab6
Emil Stjerneman <emil@stjerneman.com>
2014-04-03 11:41:03 +0200

    Renamed the script file.

---
+++ terminal_sing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function sing_lyric() {
+  if which espeak; then
+    espeak $2
+  elif which say; then
+    say $2
+  else
+    echo "You need to install say (Mac) or espeak (Unix)"
+  fi
+}
+
+sing_lyric < <(curl -fsSL https://raw.githubusercontent.com/bratanon/terminal-sing/master/lyrics/$0.lyric)
